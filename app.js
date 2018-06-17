5604e75701549be4617af64666974152c9e40633
Louis Moselhi <moselhil@gmail.com>
2018-06-17 14:57:04 +0100

    select second hand

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const secondHand = document.querySelector('.secondHand');
+
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
+  const secondDegrees = ((seconds / 60) * 360);
   console.log(seconds);
 }
