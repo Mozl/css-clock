1296d699bbb6041ab9ea1eac28629c17e2b829b6
Louis Moselhi <moselhil@gmail.com>
2018-06-17 15:17:50 +0100

    second hand working

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const secondHand = document.querySelector('.secondHand');
+const secondHand = document.querySelector('.second-hand');
 
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
-  const secondDegrees = ((seconds / 60) * 360);
+  const secondDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
   console.log(seconds);
 }
+
+setInterval(setDate, 1000);
